d1f81de25dfa711bed012703334acbb806134032
thevillanelle <krystine.hall@gmail.com>
2026-06-07 11:07:14 -0400

    feat: wrap App in ErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from '../components/ErrorBoundary'
 createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode><ErrorBoundary><App /></ErrorBoundary></StrictMode>
 )
